bef4daac78e7af597406c592805e0ed7eb22cb31
Mahirboss93370 <45433721+Mahirboss93370@users.noreply.github.com>
2020-05-11 17:49:03 +0200

    Update site.js

--- Commandes/site.js
+++ Commandes/site.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async(client, message, args) => {
-    message.channel.send("Voici le site web de Zesterium : http://zesterium.000webhostapp.com/ !");
+    message.channel.send("Voici le site web de Zesterium : https://zesterium.000webhostapp.com/ !");
 
     message.guild.channels.get("673532397257490433").send(message.author + " a utiliser la commande **/site**").catch(console.error);
 };
